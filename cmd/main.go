41500466e591658d56721cddbd0297d238368d82
Marek Aufart <maufart@redhat.com>
2022-09-20 15:45:41 +0200

    add CR for addon

--- cmd/main.go
+++ cmd/main.go
@@ -23,8 +23,11 @@ func main() {
 	addon.Run(func() (err error) {
 		//
 		// Get the addon data associated with the task.
-		fmt.Println("Addon executed - implement me!")
 		// https://github.com/konveyor/tackle2-addon-windup might be useful as an example
+		fmt.Println("Addon executed - implement me!")
+		app, _ := addon.Task.Application()
+		fmt.Printf("Triggered for application: %d %s", app.ID, app.Name)
+		fmt.Printf("With data: %v", addon.Task.Data())
 		return
 	})
 }
